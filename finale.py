afc05732a02f9978682e8b8cea7d8a16fd39ca74
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2023-03-17 07:42:52 +0300

    Update finale.py

--- finale.py
+++ finale.py
@@ -1,3 +1,4 @@
 import os
 print('Установка завершена! Для запуска прописываем python start.py')
 os.system("rm create_files.py")
+os.system('alias termtools="cd ~/krrsnk-termtools && python start.py"') 
